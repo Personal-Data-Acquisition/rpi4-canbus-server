78959037f807803eabb28b8f55a443dc240834b4
jakeg00dwin <jakegoodwin@gorge.works>
2024-02-22 00:56:40 -0800

    Updated the setup script to get the newest bcm2835 code(1.75) that has SPI bug fixes from v1.60.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
+BCM_VER='1.75'
 # Define the lines to be added
 lines_to_add="dtparam=spi=on
 dtoverlay=mcp2515-can1,oscillator=16000000,interrupt=25
@@ -25,10 +26,9 @@ echo -e "$lines_to_add" | sudo tee -a /boot/config.txt
 echo "Lines added to /boot/config.txt successfully."
 
 #https://www.waveshare.com/wiki/2-CH_CAN_HAT
-
-wget http://www.airspayce.com/mikem/bcm2835/bcm2835-1.60.tar.gz
-tar zxvf bcm2835-1.60.tar.gz 
-cd bcm2835-1.60/
+wget http://www.airspayce.com/mikem/bcm2835/bcm2835-${BCM_VER}.tar.gz
+tar zxvf bcm2835-${BCM_VER}.tar.gz 
+cd bcm2835-${BCM_VER}/
 sudo ./configure
 sudo make
 sudo make check
@@ -36,6 +36,9 @@ sudo make install
 
 
 sudo apt-get install can-utils
+
+# Allows on pi sessions and terminal multiplexing(useful for can testing)
+sudo apt install tmux
 
 echo "Install complete, rebooting to enable spi"
 sleep 5
